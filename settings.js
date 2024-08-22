f4c935f1116a81908bcad13e9e4807509761cbd4
Mastaa-md <dloan2040@gmail.com>
2024-08-22 03:55:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "22898133388"
+global.ownernomer = "24160430430"
 global.ownername = "Dave-𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
 global.ytname = "Yt: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
